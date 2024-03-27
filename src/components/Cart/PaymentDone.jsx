8dfb54f235c07e91536b107bb4d738a49c95f2a2
KetanM9555 <134033159+KetanM9555@users.noreply.github.com>
2024-03-27 14:31:38 +0530

    change the order route

--- src/components/Cart/PaymentDone.jsx
+++ src/components/Cart/PaymentDone.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const PaymentDone = () => {
-  return <section className="paymentSucess">
+  return (
+    <section className="paymentSucess">
       <main>
-          <h1>Order Confirmed</h1>
-          <p>Order placed sucessfully, You can check your order status given below</p>
-          <Link to="/myorders">Check Status</Link>
+        <h1>Order Confirmed</h1>
+        <p>
+          Order placed sucessfully, You can check your order status given below
+        </p>
+        <Link to="/orders">Check Status</Link>
       </main>
-  </section>;
+    </section>
+  );
 };
 
 export default PaymentDone;
